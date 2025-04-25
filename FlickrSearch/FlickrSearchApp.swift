79ec8e4032df34c87a844ed4fd625f9ac2d0c9b4
Aeerien <aeerien@icloud.com>
2025-04-25 21:52:53 +0300

    Initial Commit

---
+++ FlickrSearch/FlickrSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrSearchApp.swift
+//  FlickrSearch
+//
+//  Created by Irina Arkhireeva on 25.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlickrSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
